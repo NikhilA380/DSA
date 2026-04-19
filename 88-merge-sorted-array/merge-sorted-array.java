fc75b81ec15a4ea725f9caeb1e2dbcab2892fc8a
Nikhil Nagrare <nagrarenikhil27@gmail.com>
2026-04-19 23:48:46 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (16.34%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;        // pointer for nums1 (last real element)
+        int j = n - 1;        // pointer for nums2 (last element)
+        int k = m + n - 1;    // pointer for last position in nums1
+
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
+    }
+}
